141e9bb69dabe6a10c84f72439ad2f80a7c14d31
keynneravanser <keynneravanser@gmail.com>
2026-06-23 13:56:48 -0500

    fix: admin nuevas funciones a caja admin

--- backend/caja/admin.py
+++ backend/caja/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Caja, MovimientoCaja
+from .models import Caja, MovimientoCaja, CierreCaja, TrasladoDinero
 
 @admin.register(Caja)
 class CajaAdmin(admin.ModelAdmin):
-    list_display = ['bodega', 'saldo_actual']
-    readonly_fields = ['saldo_actual']
+    list_display = ['bodega', 'saldo_actual', 'abierta']
+    fields = ['bodega', 'saldo_actual', 'abierta']  # saldo_actual editable
 
 @admin.register(MovimientoCaja)
 class MovimientoCajaAdmin(admin.ModelAdmin):
-    list_display = ['caja', 'tipo', 'valor', 'descripcion', 'creado_por', 'fecha']
-    readonly_fields = ['fecha']
+    list_display = ['caja', 'tipo', 'valor', 'descripcion', 'fecha']
+
+@admin.register(CierreCaja)
+class CierreCajaAdmin(admin.ModelAdmin):
+    list_display = ['caja', 'fecha', 'saldo_teorico', 'saldo_fisico', 'diferencia']
+
+@admin.register(TrasladoDinero)
+class TrasladoDineroAdmin(admin.ModelAdmin):
+    list_display = ['caja_origen', 'caja_destino', 'valor', 'creado_en']
